77aa0863fc67040e2bd41a12f4a1f18cfe7bcc4d
Anna <turbina.anna97@yandex.ru>
2022-12-05 12:00:38 +0100

    new code

--- Program27/Program27.cs
+++ Program27/Program27.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+ static int digSum(int n)
+    {
+        int sum = 0;
+        while (n != 0) 
+        {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Сумма всех цифр числа {n} = {digSum(n)}");
+    }
+Main();
+
+
