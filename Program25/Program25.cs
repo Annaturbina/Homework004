b34f3136481d6afb6224ba60dc355992c0f6da21
Anna <turbina.anna97@yandex.ru>
2022-12-05 10:29:45 +0100

    new code

--- Program25/Program25.cs
+++ Program25/Program25.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+ToDegree(a, b);
+void ToDegree(int a, int b)
+{
+    double result = 1;
+    {
+        result = result * Math.Pow(a, b);
+    }
+    Console.WriteLine($"{a}^{b}={result}");
+}
