c3d76b5b5d3d795a1bf3b85113582a38e4744073
Anna <turbina.anna97@yandex.ru>
2022-12-05 12:02:14 +0100

    new code

--- Program27/Program27.cs
+++ Program27/Program27.cs
@@ -1,19 +1,23 @@
-﻿Console.WriteLine("Введите число:");
- static int digSum(int n)
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        int sum = 0;
-        while (n != 0) 
+        Console.WriteLine("Введите число:");
+        static int digSum(int n)
         {
-            sum = sum + n % 10;
-            n = n / 10;
+            int sum = 0;
+            while (n != 0)
+            {
+                sum = sum + n % 10;
+                n = n / 10;
+            }
+            return sum;
         }
-        return sum;
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма всех цифр числа {n} = {digSum(n)}");
+        }
+        Main();
     }
-    static void Main()
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Сумма всех цифр числа {n} = {digSum(n)}");
-    }
-Main();
-
-
+}
