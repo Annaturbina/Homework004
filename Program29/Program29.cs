b6936e50e401c4a06c6cac18e227be06c35e5580
Anna <turbina.anna97@yandex.ru>
2022-12-05 12:27:48 +0100

    new code

--- Program29/Program29.cs
+++ Program29/Program29.cs
@@ -1,19 +1,25 @@
-﻿Console.WriteLine("Введите 8 целых чисел через пробел:");
-static void Arr()
+﻿internal class Program
 {
-    string s = Console.ReadLine();
-    string[] line = s.Split(' ');
-    int[] array = new int[line.Length];
-    Console.Write("[");
-    for (int i = 0; i < line.Length - 1; i++)
+    private static void Main(string[] args)
     {
-        array[i] = int.Parse(line[i]);
-        Console.Write($"{array[i]}, ");
-    }
-    for (int i = line.Length - 1; i < line.Length; i++)
-    {
-        array[i] = int.Parse(line[i]);
-        Console.Write($"{array[i]}]");
+        Console.WriteLine("Введите 8 целых чисел через пробел:");
+        static void Arr()
+        {
+            string s = Console.ReadLine();
+            string[] line = s.Split(' ');
+            int[] array = new int[line.Length];
+            Console.Write("[");
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                array[i] = int.Parse(line[i]);
+                Console.Write($"{array[i]}, ");
+            }
+            for (int i = line.Length - 1; i < line.Length; i++)
+            {
+                array[i] = int.Parse(line[i]);
+                Console.Write($"{array[i]}]");
+            }
+        }
+        Arr();
     }
 }
-Arr();
